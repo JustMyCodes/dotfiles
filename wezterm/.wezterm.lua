802d22657afe24724d8359f5475710acfc7e2411
Henrique Rodrigues <henrique@justmy.codes>
2026-07-12 15:45:15 -0300

    fix(wezterm): ajuste de fonte e janela para uso na máquina pessoal

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -25,11 +25,11 @@ config.color_scheme = 'Navy and Ivory (terminal.sexy)'
 config.initial_cols = 120                           -- Configura o tamanho inicial da janela
 config.initial_rows = 28
 config.enable_tab_bar = false                       -- Remove a barra de abas para visual mais limpo
-config.window_decorations = 'RESIZE'                -- Usar NONE ou RESIZE para desativar a barra de título
+config.window_decorations = 'NONE'                  -- Usar NONE ou RESIZE para desativar a barra de título
 
 -- Fonte ---------------------------------------------------------------
 config.font = wezterm.font( 'MesloLGM Nerd Font Mono' )
-config.font_size = 10
+config.font_size = 13
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }     -- Desabilita legatures da Fonte
 
 
